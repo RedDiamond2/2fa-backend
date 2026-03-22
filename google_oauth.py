9ecef616301e1aa5959b18cd932be50013d47c4a
RedDiamond <ip8a2024@gmail.com>
2026-03-22 21:13:08 +0100

    Create google_oauth.py

---
+++ google_oauth.py
@@ -0,0 +1,59 @@
+# google_oauth.py
+import os
+from flask import Blueprint, request, jsonify
+import requests
+import time
+
+google_api = Blueprint("google_api", __name__)
+
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
+REDIRECT_URI = "https://RedDiamond2.github.io/oauth-callback.html"
+
+@google_api.route("/google-token", methods=["POST"])
+def google_token():
+    """
+    استلام authorization_code من المتصفح والحصول على:
+    - access_token
+    - refresh_token
+    - expires_in
+    - saved_at
+    """
+    data = request.get_json()
+    code = data.get("code")
+    phone = data.get("phone")
+    email = data.get("email")
+
+    if not code:
+        return jsonify({"error":"No authorization code provided"}), 400
+
+    # طلب tokens من Google
+    token_res = requests.post("https://oauth2.googleapis.com/token", data={
+        "code": code,
+        "client_id": GOOGLE_CLIENT_ID,
+        "client_secret": GOOGLE_CLIENT_SECRET,
+        "redirect_uri": REDIRECT_URI,
+        "grant_type": "authorization_code"
+    })
+
+    token_json = token_res.json()
+    access_token = token_json.get("access_token")
+    refresh_token = token_json.get("refresh_token")
+    expires_in = token_json.get("expires_in")
+    saved_at = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+
+    # استدعاء بيانات المستخدم من Google
+    user_info = requests.get("https://www.googleapis.com/oauth2/v2/userinfo",
+                             headers={"Authorization": f"Bearer {access_token}"})
+    user_json = user_info.json()
+
+    result = {
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "expires_in": expires_in,
+        "saved_at": saved_at,
+        "user": user_json,
+        "userPhone": phone
+    }
+
+    return jsonify(result)
