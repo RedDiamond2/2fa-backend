5b3c38c47490d432559cbf24cdf6fe4b5d289e69
RedDiamond <ip8a2024@gmail.com>
2026-03-30 13:45:35 +0200

    Create mongo_db.py

---
+++ mongo_db.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+import os
+
+# إعداد الاتصال (تأكد من وضع URI الخاص بك في إعدادات Render)
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://ip8a2024_db_user:t5Qq8VSmpSHAOqca@cluster0.eqswpgj.mongodb.net")
+client = MongoClient(MONGO_URI)
+db = client['RedDiamondDB']
+
+# المجموعات الأساسية
+users_col = db['users']           # بيانات المستخدمين والأرصدة
+transactions_col = db['transactions'] # سجل العمليات (غير قابل للتعديل)
+referrals_col = db['referrals']   # سجل الإحالات
